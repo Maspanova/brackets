607b97765c152cae575ddae0e350894e2b60837a
Maspanova <105561456+Maspanova@users.noreply.github.com>
2023-02-16 19:52:08 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const config = bracketsConfig.map(el => el.join(''));
+  for (let i = 0; i < config.length;) {
+    !str.includes(config[i]) ? i++ : (str = str.replace(config[i], ''), i = 0)
+  }
+  return str.length === 0 ? true : false
+} // your solution
